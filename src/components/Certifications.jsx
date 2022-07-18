db8d6e7866ca5a70c111d7f548df07a0cf40f55a
Bryanmartz972 <bryanmartz972@gmail.com>
2022-07-17 23:34:31 -0600

    NEW: Github Pages package added

---
+++ src/components/Certifications.jsx
@@ -0,0 +1,14 @@
+import ResponsiveDiv from './ResponsiveDiv'
+
+const Certifications = () => {
+	return (
+		<section className='flex flex-col items-center py-8'>
+			<h3 className='font-bold text-2xl self-center'>Certifications</h3>
+			<ResponsiveDiv>
+				<div></div>
+			</ResponsiveDiv>
+		</section>
+	)
+}
+
+export default Certifications
