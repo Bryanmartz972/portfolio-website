eab748433eb6442b356b00e546007178a69167f3
Bryanmartz972 <bryanmartz972@gmail.com>
2022-08-09 12:03:26 -0600

    👌IMPROVE: ProjectCard Github button alignment

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,6 +1,6 @@
 const PageLayout = ({children}) => {
   return (
-		<div className= 'mb-[54px] sm:mb-0 sm:pt-[62px] w-full bg-background text-white'>
+		<div className= 'mb-[54px] sm:mb-0 sm:pt-[62px] w-full bg-background text-white cursor-default'>
 			{children}
 		</div>
 	)
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { GithubButton } from './Button'
 
 const ProjectCard = ({ title, info, text, image, linkTo }) => {
 	return (
-		<div className='bg-primary w-full rounded-xl'>
+		<div className='bg-primary flex flex-col w-full justify-between rounded-xl'>
 			<div className='flex flex-row w-full p-4'>
 				<div className='w-3 h-3 mr-2 rounded-full bg-[#ff605c]'></div>
 				<div className='w-3 h-3 mr-2 rounded-full bg-[#ffbd44]'></div>
@@ -17,7 +17,7 @@ const ProjectCard = ({ title, info, text, image, linkTo }) => {
 					loading='lazy'
 				/>
 			</div>
-			<div className='flex flex-col px-2 pb-4'>
+			<div className='flex flex-col px-2'>
 				<h3 className='font-bold text-2xl py-2'>{title}</h3>
 				<div className='grid grid-cols-2 gap-x-4 gap-y-2 py-2 p'>
 					{text.map((tech, index) => {
@@ -25,9 +25,9 @@ const ProjectCard = ({ title, info, text, image, linkTo }) => {
 					})}
 				</div>
 				<div className='mt-2 mb-4'>{info}</div>
-				<div className=' '>
-					<GithubButton linkTo={linkTo} />
-				</div>
+			</div>
+			<div className='px-2 pb-4'>
+				<GithubButton linkTo={linkTo} />
 			</div>
 		</div>
 	)
