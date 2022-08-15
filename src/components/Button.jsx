0a0927cbe5b58846ef087961981d60dbf2b329c4
Bryanmartz972 <bryanmartz972@gmail.com>
2022-08-14 21:10:27 -0600

    📦NEW: New ButtonForm made with button tag

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,9 +7,19 @@ const Button = ({ linkTo, text }) => {
 			target='_blank'
 			rel='noopener noreferrer'
 			href={linkTo}
-			className='flex items-center justify-center bg-primary rounded-xl py-3 sm:py-2 px-4 font-bold hover:shadow-white/10 hover:shadow-lg hover:bg-secondary transition-all'>
+			className='flex items-center justify-center bg-primary rounded-xl py-3 sm:py-2 px-4 font-bold hover:shadow-white/10 hover:shadow-lg hover:bg-secondary transition-all hover:cursor-pointer'>
 			{text}
 		</a>
+	)
+}
+
+const ButtonForm = ({ clickEvent, children }) => {
+	return (
+		<button
+			onClick={clickEvent}
+			className='flex items-center justify-center bg-primary rounded-xl py-3 sm:py-2 px-4 font-bold hover:shadow-white/10 hover:shadow-lg hover:bg-secondary transition-all hover:cursor-pointer'>
+			{children}
+		</button>
 	)
 }
 
@@ -19,7 +29,7 @@ const GithubButton = ({ linkTo }) => {
 			href={linkTo}
 			rel='noopener noreferrer'
 			target='_blank'
-			className='flex cursor-pointer items-center justify-center bg-[#161b22] rounded-xl py-3 sm:py-2 px-4 font-bold hover:shadow-white/10 hover:shadow-lg hover:bg-white hover:text-black transition-all'>
+			className='flex cursor-pointer items-center justify-center bg-[#161b22] rounded-xl py-3 sm:py-2 px-4 font-bold hover:shadow-white/10 hover:shadow-lg hover:bg-white hover:text-black transition-all hover:cursor-pointer'>
 			<FaGithub size='1rem' /> <span className='ml-2'>Github</span>
 		</a>
 	)
@@ -34,4 +44,9 @@ GithubButton.propTypes = {
 	linkTo: PropTypes.string.isRequired,
 }
 
-export { Button, GithubButton }
+ButtonForm.propTypes = {
+	clickEvent: PropTypes.func.isRequired,
+	children: PropTypes.node.isRequired,
+}
+
+export { Button, GithubButton, ButtonForm }
