acd564b39e454ba4fc060a222217b1489f8bbfb5
Bryanmartz972 <bryanmartz972@gmail.com>
2022-10-06 09:21:49 -0600

    NEW: Added data-cy attributes to Header elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,27 @@ import BryanLogo from '../assets/images/header/bryan-logo.webp'
 const Header = () => {
 	return (
 		<header className='hidden w-full h-[62px] sm:z-10 sm:flex sm:fixed bg-primary text-white items-center justify-between px-8 font-black'>
-			<a href='#home'>
+			<a href='#home' data-cy='logo-link'>
 				<img src={BryanLogo} alt='' className='h-[62px] object-none' />
 			</a>
 			<nav>
 				<ul className='flex'>
-					<HeaderLink linkTo={'#about-me'}>About</HeaderLink>
-					<HeaderLink linkTo={'#skills'}>Skills</HeaderLink>
-					<HeaderLink linkTo={'#projects'}>Projects</HeaderLink>
+					<HeaderLink linkTo='#about-me' dataAttribute='aboutme-link'>
+						About
+					</HeaderLink>
+					<HeaderLink linkTo='#skills' dataAttribute='skills-link'>
+						Skills
+					</HeaderLink>
+					<HeaderLink linkTo='#projects' dataAttribute='projects-link'>
+						Projects
+					</HeaderLink>
 				</ul>
 			</nav>
 			<div>
 				<a
 					className='flex bg-secondary p-2 rounded-xl items-center justify-center cursor-pointer hover:text-secondary hover:bg-white transition-all'
-					href='#contact-me'>
+					href='#contact-me'
+					data-cy='contactme-link'>
 					Contact me!
 				</a>
 			</div>
--- src/components/HeaderLink.jsx
+++ src/components/HeaderLink.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types'
 
-const HeaderLink = ({ linkTo, children }) => {
+const HeaderLink = ({ linkTo, children, dataAttribute }) => {
 	return (
 		<a
 			className='py-2 px-6 mx-2 rounded-xl  hover:bg-secondary cursor-pointer hover:transition-colors'
-			href={linkTo}>
+			href={linkTo}
+			data-cy={dataAttribute}>
 			{children}
 		</a>
 	)
